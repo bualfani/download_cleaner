7665c7ff29bb767a36429fe25837f251bcc50496
bahati <bulozealfani@gmail.com>
2023-03-29 21:36:34 -0500

    add while loop and amount of time tell checking again

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ folder_path = "/path/to/Downloads/folder"
 
 # replace with the number of days to keep files
 days_threshold = 30
+
+while True:
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        if os.stat(file_path).st_mtime < time.time() - days_threshold * 86400:
+            os.remove(file_path)
+
+    # Wait 5 minutes before checking again
+    time.sleep(300)
