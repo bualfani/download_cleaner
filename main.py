94279a0a2e2a4ebfa21ac3d19440f0649941b3e3
bahati <bulozealfani@gmail.com>
2023-03-29 21:33:54 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+# replace with your Downloads folder path
+folder_path = "/path/to/Downloads/folder"
+
+# replace with the number of days to keep files
+days_threshold = 30
